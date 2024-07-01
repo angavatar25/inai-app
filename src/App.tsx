3f56ae0556b563ea0adf40be1519cdad6a7466b7
Ananda Afryan Gemilang <anandalark007@gmail.com>
2024-07-01 09:48:35 +0700

    feat: router and packages configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Dashboard from './pages/Dashboard';
+import NetAssets from './pages/NetAssets';
+
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+
+const AppRouter = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Dashboard/> },
+    { path: 'net-assets', element: <NetAssets/> }
+  ])
+
+  return routes;
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppRouter/>
+    </Router>
   );
 }
 
---
+++ src/pages/NetAssets/index.tsx
@@ -0,0 +1,7 @@
+const NetAssets = () => {
+  return (
+    <p>Net Assets</p>
+  )
+};
+
+export default NetAssets
