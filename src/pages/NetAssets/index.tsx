120fbe3409a9c2445ea60402927841514fb0088f
Ananda Afryan Gemilang <anandalark007@gmail.com>
2024-07-07 00:49:56 +0700

    feat: add navbar and desktop responsive

--- src/pages/NetAssets/index.tsx
+++ src/pages/NetAssets/index.tsx
@@ -43,56 +43,60 @@ const NetAssets = () => {
 
   return (
     <DashboardContainer>
-      <Title $fontSize={24}>Asset balances</Title>
-      <Button>
-        <FontAwesomeIcon icon={faMagicWandSparkles}/>
-        Ask AI assistant
-        <FontAwesomeIcon icon={faArrowRight}/>
-      </Button>
-      <div style={{ marginBottom: '20px' }}>
-        <Categories
-          onClickCategory={onClickCountryCategory}
-        />
-      </div>
-      <AssetsBalance
-        currentCountry={currentCountry}
-      />
-      <FlexBetween>
-        <p>Group By</p>
-        <div>
-          <PerformanceCategories
-            filterData={filterAssetsData}
-            onClickCategory={onClickPortfolioCategory}
-            currentTab={currentAssets}
+      <div>
+        <Title $fontSize={24}>Asset balances</Title>
+        <Button>
+          <FontAwesomeIcon icon={faMagicWandSparkles}/>
+          Ask AI assistant
+          <FontAwesomeIcon icon={faArrowRight}/>
+        </Button>
+        <div style={{ marginBottom: '20px' }}>
+          <Categories
+            onClickCategory={onClickCountryCategory}
           />
         </div>
-      </FlexBetween>
-      <AssetsList
-        data={currentAssetsData}
-      />
-      <Title
-        $fontSize={16}
-        $color="#000"
-        style={{ marginTop: '20px', fontWeight: 700 }}
-      >
-        AI suggested queries
-      </Title>
-      <div style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', gap: '10px' }}>
-        {suggestedQueries.map((text) => (
-          <ButtonComponent
-            key={`suggestion-${text}`}
-            bgColor="#fff"
-            color="#475569"
-            borderColor="#CBD5E1"
-            lineHeight={40}
-            fontSize={14}
-            fontWeight={700}
-            style={{ justifyContent: 'left', paddingLeft: '20px' }}
-          >
-            <FontAwesomeIcon icon={faMagicWandSparkles}/>
-            {text}
-          </ButtonComponent>
-        ))}
+        <AssetsBalance
+          currentCountry={currentCountry}
+        />
+      </div>
+      <div>
+        <FlexBetween>
+          <p>Group By</p>
+          <div>
+            <PerformanceCategories
+              filterData={filterAssetsData}
+              onClickCategory={onClickPortfolioCategory}
+              currentTab={currentAssets}
+            />
+          </div>
+        </FlexBetween>
+        <AssetsList
+          data={currentAssetsData}
+        />
+        <Title
+          $fontSize={16}
+          $color="#000"
+          style={{ marginTop: '20px', fontWeight: 700 }}
+        >
+          AI suggested queries
+        </Title>
+        <div style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', gap: '10px' }}>
+          {suggestedQueries.map((text) => (
+            <ButtonComponent
+              key={`suggestion-${text}`}
+              bgColor="#fff"
+              color="#475569"
+              borderColor="#CBD5E1"
+              lineHeight={40}
+              fontSize={14}
+              fontWeight={700}
+              style={{ justifyContent: 'left', paddingLeft: '20px' }}
+            >
+              <FontAwesomeIcon icon={faMagicWandSparkles}/>
+              {text}
+            </ButtonComponent>
+          ))}
+        </div>
       </div>
     </DashboardContainer>
   )
--- src/styles/components/NavBar.ts
+++ src/styles/components/NavBar.ts
@@ -13,7 +13,27 @@ export const NavbarContainer = styled.div`
 export const NavbarTitle = styled.p`
   font-size: 20px;
   font-weight: 800;
+  color: #000;
 `
 export const TitleSpan = styled.span`
   color: #4f46e5;
+`
+
+export const NavbarDesktopContainer = styled.div`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  min-height: 100vh;
+  padding: 30px;
+  background-color: #fff;
+  color: white;
+  padding: 10px;
+  box-sizing: border-box;
+  flex-shrink: 0;
+  box-shadow: 5px 20px 10px 2px #1717170F;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: stretch;
+
 `
